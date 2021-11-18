85d0a455a49dabb17576a7ffb9e29109dded800d
FabioCividini <fabiocividini22@gmail.com>
2021-11-18 15:43:13 +0100

    lavoro finito

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -1,11 +1,32 @@
 package org.generation.italy.cicli;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class StampaNumeriDispari {
 
 	public static void main(String[] args) {
 		
+			Scanner scanner = new Scanner(System.in);
+			Random random = new Random();
+			
+			System.out.print("Scegli la lunghezza del Array: ");
+			int lunghezza = scanner.nextInt();
+			
+			int[] nArray = new int[lunghezza];
+			
+			System.out.println("Numeri dispari: ");			
+			
+			for(int i = 0; i < lunghezza; i ++) {
+					nArray[i] = random.nextInt(100);
+					
+					if(nArray[i] % 2 != 0) {
+						System.out.print(nArray[i] + " ");
+					}
+			}
+			
+			scanner.close();
 		
-
 	}
-
+		
 }
